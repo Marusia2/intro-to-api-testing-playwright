d320d57a502bb009565f9bb32ea1fec76abc0f4a
Marija <gestuverteja.marija@gmail.com>
2024-10-24 20:47:29 +0300

    code formatting

--- tests/auth-flow.spec.ts
+++ tests/auth-flow.spec.ts
@@ -5,37 +5,25 @@ import { LoginDto } from './dto/login-dto'
 const serviceURL = 'https://backend.tallinn-learning.ee/'
 const loginPath = 'login/student'
 
-test('incorrect username and password return 401 ', async ({
-                                                                                       request,
-                                                                                     }) => {
-  const loginDto =
-    LoginDto.createLoginWithIncorrectCredentials()
+test('incorrect username and password return 401 ', async ({ request }) => {
+  const loginDto = LoginDto.createLoginWithIncorrectCredentials()
 
-  const response = await request.post(
-    `${serviceURL}${loginPath}`,
-    {
-      data: loginDto,
-    },
-  )
+  const response = await request.post(`${serviceURL}${loginPath}`, {
+    data: loginDto,
+  })
   console.log('response status:', response.status())
   expect.soft(response.status()).toBe(StatusCodes.UNAUTHORIZED)
-
 })
 
 test('successful authorization flow with correct username and password return 200', async ({
-                                                     request,
-                                                   }) => {
-  const loginDto =
-    LoginDto.createLoginWithCorrectCredentials()
+  request,
+}) => {
+  const loginDto = LoginDto.createLoginWithCorrectCredentials()
 
-  const response = await request.post(
-    `${serviceURL}${loginPath}`,
-    {
-      data: loginDto,
-    },
-  )
+  const response = await request.post(`${serviceURL}${loginPath}`, {
+    data: loginDto,
+  })
   console.log('response status:', response.status())
   expect.soft(response.status()).toBe(StatusCodes.OK)
-  console.log( await response.text())  //async
-
+  console.log(await response.text()) //async
 })
